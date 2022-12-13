b3efceefd05c2a66f515326e95ef6bbe0b9a955f
ca r215.07 <ca.r215.07@ca.local>
2022-12-13 01:49:05 -0800

    first commit

---
+++ Layihe-Task/Models/Weapon.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layihe_Task.Models
+{
+    internal class Weapon
+    {
+        public int _capacity;
+        public int _count = 70;
+        public double _dischargeSecond;
+        public bool _shootMode;
+
+        
+        public int Capacity
+        {
+            get => _capacity;
+            set 
+            { 
+                if(_capacity > 0) 
+                { 
+                    _capacity = value; 
+                }
+            }
+        }
+        
+        public int Count
+        {
+            get => _count;
+            set 
+            { 
+                if(_count > 0)
+                {
+                    _count = value;
+                }
+            }
+        }
+
+        
+        public double DischargeSecond
+        {
+            get => _dischargeSecond;
+            set 
+            { 
+                _dischargeSecond = value; 
+            }
+        }
+
+        
+        public bool ShootMode
+        {
+            get => _shootMode;
+            set 
+            { 
+                _shootMode = value; 
+            }
+        }
+        public Weapon(int capacity, int count, double dischargeSecond, bool shootMode)
+        {
+            Capacity = capacity;
+            Count = count;
+            DischargeSecond = dischargeSecond;
+            ShootMode = shootMode;
+        }
+
+        public void Shoot()
+        {
+            
+                if(Count > 0)
+                {
+                    _count --;
+                    Console.WriteLine("say: "+ _count);
+                }
+                else
+                {
+                    Console.WriteLine("Silahda hewne yoxdu");
+                }
+        
+            
+        }
+        public void Fire()
+        {
+            Count = 0;
+            var second =(Capacity / DischargeSecond) * Count;
+            Console.WriteLine("Saniye bitdiyi qeyd olunur");
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine(Capacity - Count);
+        }
+        public void Reload()
+        {
+            Count = Capacity;
+            Console.WriteLine("Yenilendi");
+            string cavab = Console.ReadLine();
+            if (cavab.ToLower().Trim() == null)
+            {
+                Console.WriteLine("");
+            }
+            else if(cavab.ToLower().Trim() != "0")
+            {
+                Console.WriteLine("");
+            }
+        }
+        public void ChangeFireMode()
+        {
+            if(ShootMode == true)
+            {
+                if(Count >0)
+                {
+                    Fire();
+                    Console.WriteLine("Mode istifade eliyir");
+                }
+                else
+                {
+                    Console.WriteLine("Silahda hewne yoxdu");
+                }
+            }
+        }
+    }
+}
---
+++ Layihe-Task/Program.cs
@@ -0,0 +1,48 @@
+﻿using Layihe_Task.Models;
+
+namespace Layihe_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Weapon weapon = new Weapon(30,70,10,true);
+            
+            do
+            {
+                Console.WriteLine("0.Informasiya almag ucun \n1.Shoot metodu ucun \n2.Fire metodu ucun \n3.GetRemainBulletCount metodu ucun \n4.Reload metodu ucun \n5.ChangeFireMode \n6.Proqramdan dayandirmag ucun qosayoldur");
+                Console.WriteLine("Seciminizi edin: ");
+            byte input = Convert.ToByte(Console.ReadLine());
+
+                switch (input)
+                {
+                    case 0:
+                        Console.WriteLine("Informasiya almag ucun");
+                        break;
+                    case 1: 
+                        Console.WriteLine("Shoot metodu ucun");
+                        weapon.Shoot();
+                        break;
+                        case 2:
+                        Console.WriteLine("Fire metodu ucun");
+                        weapon.Fire();
+                        break;
+                        case 3:
+                        Console.WriteLine("GetRemainBulletCount");
+                        weapon.GetRemainBulletCount();
+                        break;
+                        case 4:
+                        Console.WriteLine("Reload metodu ucun");
+                        weapon.Reload();
+                        break;
+                        case 5:
+                        Console.WriteLine("ChangeFireMode");
+                        weapon.ChangeFireMode();
+                        break;
+                }
+                
+            } while (true);
+        }
+    }
+}
